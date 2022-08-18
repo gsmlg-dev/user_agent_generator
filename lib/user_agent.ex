9594a39c3283f533e8dd55fbca3dc2b0efe85d5e
Jonathan <3713684+gsmlg@users.noreply.github.com>
2022-08-18 12:46:54 +0800

    fix: Fix random and random_all.

--- lib/user_agent.ex
+++ lib/user_agent.ex
@@ -2,14 +2,14 @@ defmodule UserAgent do
   @doc "Return a random user agent from most common user agents"
   def random() do
     len = UserAgent.Data.most_common() |> length
-    r = Enum.random(0..len)
+    r = Enum.random(0..(len - 1))
     UserAgent.Data.most_common() |> Enum.at(r)
   end
 
   @doc "Return a random user agent from all agents"
   def random_all() do
     len = UserAgent.Data.all_agents() |> length
-    r = Enum.random(0..len)
+    r = Enum.random(0..(len - 1))
     UserAgent.Data.all_agents() |> Enum.at(r) |> Map.get(:name)
   end
 end
